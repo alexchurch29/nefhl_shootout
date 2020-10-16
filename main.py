85484eaf0659cffc1a1ce26bda8d5af6a35bd7f8
Alex Church <35304852+alexchurch29@users.noreply.github.com>
2020-10-15 22:48:24 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import random
 
 def parse_skaters(n=20):
     '''
@@ -15,16 +16,13 @@ def parse_skaters(n=20):
     skaters = skaters.rename(columns={"Att.": "Attempts", "Made": "Goals"})
     skaters = skaters.groupby('Player').sum()
     skaters['Actual_Conversion_Rate'] = skaters['Goals'] / skaters['Attempts']
-    # league_avg = skaters['Goals'].sum() / skaters['Attempts'].sum()
     skaters['Estimated_Conversion_Rate'] = np.where(skaters['Attempts'] >= n, skaters['Actual_Conversion_Rate'],
                                                     (skaters['Actual_Conversion_Rate'] * (skaters['Attempts'] / n) +
                                                       league_median * ((n - skaters['Attempts'])/ n)))
     skaters = skaters.sort_values(by=['Estimated_Conversion_Rate'], ascending=False)
-    skaters['SO_Rating'] = 50 + 50 * skaters['Estimated_Conversion_Rate']
 
-    # print(skaters)
-    skaters.to_csv('skaters_test.csv')
-    return
+    # skaters.to_csv('skaters_test.csv')
+    return skaters
 
 
 def parse_goalies(n=80):
@@ -41,18 +39,45 @@ def parse_goalies(n=80):
     goalies = goalies.rename(columns={"Att.": "Attempts", "Miss": "Saves"})
     goalies = goalies.groupby('Player').sum()
     goalies['Actual_Save_Rate'] = goalies['Saves'] / goalies['Attempts']
-    # league_avg = goalies['Saves'].sum() / goalies['Attempts'].sum()
     goalies['Estimated_Save_Rate'] = np.where(goalies['Attempts'] >= n, goalies['Actual_Save_Rate'],
                                                     (goalies['Actual_Save_Rate'] * (goalies['Attempts'] / n) +
                                                       league_median * ((n - goalies['Attempts'])/ n)))
     goalies = goalies.sort_values(by=['Estimated_Save_Rate'], ascending=False)
-    goalies['SO_Rating'] = 50 + 50 * goalies['Estimated_Save_Rate']
 
-    # print(goalies)
-    goalies.to_csv('goalies_test.csv')
-    return 
+    # goalies.to_csv('goalies_test.csv')
+    return goalies
 
 
 if __name__ == '__main__':
-    parse_skaters()
-    parse_goalies()
+    skaters = parse_skaters()
+    goalies = parse_goalies()
+
+    skaters = pd.DataFrame([skaters.ix[idx] for idx in skaters.index for _ in range(int(skaters.ix[idx]['Attempts']))]).reset_index(drop=True)
+    goalies = pd.DataFrame([goalies.ix[idx] for idx in goalies.index for _ in range(int(goalies.ix[idx]['Attempts']))]).reset_index(drop=True)
+
+    s = skaters.shape[0]
+    g = goalies.shape[0]
+
+    goals = 0
+    saves = 0
+
+    for i in range(1000000):
+        # print('\n')
+        r1 = random.randint(0, s-1)
+        r2 = random.randint(0, g-1)
+        skater = skaters.iloc[[r1]]
+        goalie = goalies.iloc[[r2]]
+        # print(skaters.iloc[r1])
+        # print(goalies.iloc[r2])
+        # print('xG: ' + str(skaters.iloc[r1][3] / (goalies.iloc[r2][3] + skaters.iloc[r1][3])))
+        p1 = int(skaters.iloc[r1][3]*100)
+        p2 =  int(goalies.iloc[r2][3]*100)
+        r3 = random.randint(1,p1+p2)
+        if r3 <= p1:
+            # print("Result: Goal")
+            goals+=1
+        else:
+            # print("Result: No Goal")
+            saves+=1
+        # print('\n')
+    print(goals/(saves+goals))
