a34ba78c2690eb291c0e77d582bc92f6064ed996
Alex Church <35304852+alexchurch29@users.noreply.github.com>
2020-10-16 22:00:52 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def parse_skaters(n=30):
     :param n: sample size to use for regression
     :return:
     '''
-    skaters = pd.read_csv('skaters.csv')
+    skaters = pd.read_csv('skaters_raw_data.csv')
     # print(skaters['Att.'].sum())
 
     league_median = skaters['Pct.'].median() / 100
@@ -26,10 +26,15 @@ def parse_skaters(n=30):
     min_val = skaters['Estimated_Conversion_Rate'].min()
     spread = (max_val - min_val) * 100
     skaters['SO_Rating'] = 50 + (((skaters['Estimated_Conversion_Rate'] - min_val) * 100) * (49 / spread))
-    print(skaters['SO_Rating'].median())
+    print(skaters['SO_Rating'].quantile(0.25))
+
+    league_mean = skaters['Goals'].sum() / skaters['Attempts'].sum()
+    print(league_mean)
+
+    skaters['Estimated_Conversion_Rate1'] = (skaters['Estimated_Conversion_Rate'] * (1 - league_mean)) / (1 - skaters['Estimated_Conversion_Rate'])
 
     # print(skaters)
-    skaters.to_csv('skaters_test.csv')
+    # skaters.to_csv('skaters_test.csv')
     return skaters
 
 
@@ -39,7 +44,7 @@ def parse_goalies(n=50):
     :param n: sample size to use for regression
     :return:
     '''
-    goalies = pd.read_csv('goalies.csv')
+    goalies = pd.read_csv('goalies_raw_data.csv')
     # print(goalies['Att.'].sum())
 
     league_median = goalies['Pct.'].median() / 100
@@ -56,10 +61,14 @@ def parse_goalies(n=50):
     min_val = goalies['Estimated_Save_Rate'].min()
     spread = (max_val - min_val) * 100
     goalies['SO_Rating'] = 50 + (((goalies['Estimated_Save_Rate'] - min_val) * 100) * (49 / spread))
-    print(goalies['SO_Rating'].median())
+    print(goalies['SO_Rating'].quantile(0.25))
+
+    skaters = pd.read_csv('skaters.csv')
+    league_mean = skaters['Made'].sum() / skaters['Att.'].sum()
+    goalies['Estimated_Save_Rate1'] = (goalies['Estimated_Save_Rate'] * (league_mean)) / (1 - goalies['Estimated_Save_Rate'])
 
     # print(goalies)
-    goalies.to_csv('goalies_test.csv')
+    # goalies.to_csv('goalies_test.csv')
     return goalies
 
 
@@ -90,20 +99,70 @@ def simulate():
         r2 = random.randint(0, g - 1)
         skater = skaters.iloc[[r1]]
         goalie = goalies.iloc[[r2]]
-        # print(skaters.iloc[r1])
-        # print(goalies.iloc[r2])
-        # print('xG: ' + str(skaters.iloc[r1][3] / (goalies.iloc[r2][3] + skaters.iloc[r1][3])))
-        p1 = int(skaters.iloc[r1][3] * 100)
-        p2 = int(goalies.iloc[r2][3] * 100)
+        print(skaters.iloc[r1])
+        print(goalies.iloc[r2])
+        print('xG: ' + str(skaters.iloc[r1][5] / (goalies.iloc[r2][5] + skaters.iloc[r1][5])))
+        p1 = int(skaters.iloc[r1][5] * 100)
+        p2 = int(goalies.iloc[r2][5] * 100)
         r3 = random.randint(1, p1 + p2)
         if r3 <= p1:
-            # print("Result: Goal")
+            print("Result: Goal")
             goals += 1
         else:
-            # print("Result: No Goal")
+            print("Result: No Goal")
             saves += 1
-            # print('\n')
+            print('\n')
     print(goals / (saves + goals))
+    #
+    # goals = 0
+    # saves = 0
+    #
+    # r1 = random.randint(0, s - 1)
+    # print(skaters.iloc[r1])
+    #
+    # for i in range(10000):
+    #     # print('\n')
+    #     r2 = random.randint(0, g - 1)
+    #     skater = skaters.iloc[[r1]]
+    #     goalie = goalies.iloc[[r2]]
+    #     # print(goalies.iloc[r2])
+    #     # print('xG: ' + str(skaters.iloc[r1][3] / (goalies.iloc[r2][3] + skaters.iloc[r1][3])))
+    #     p1 = int(skaters.iloc[r1][5] * 100)
+    #     p2 = int(goalies.iloc[r2][5] * 100)
+    #     r3 = random.randint(1, p1 + p2)
+    #     if r3 <= p1:
+    #         # print("Result: Goal")
+    #         goals += 1
+    #     else:
+    #         # print("Result: No Goal")
+    #         saves += 1
+    #         # print('\n')
+    # print(goals / (saves + goals))
+    #
+    # goals = 0
+    # saves = 0
+    #
+    # r2 = random.randint(0, g - 1)
+    # print(goalies.iloc[r2])
+    #
+    # for i in range(10000):
+    #     # print('\n')
+    #     r1 = random.randint(0, s - 1)
+    #     skater = skaters.iloc[[r1]]
+    #     goalie = goalies.iloc[[r2]]
+    #     # print(skaters.iloc[r1])
+    #     # print('xG: ' + str(skaters.iloc[r1][3] / (goalies.iloc[r2][3] + skaters.iloc[r1][3])))
+    #     p1 = int(skaters.iloc[r1][5] * 100)
+    #     p2 = int(goalies.iloc[r2][5] * 100)
+    #     r3 = random.randint(1, p1 + p2)
+    #     if r3 <= p1:
+    #         # print("Result: Goal")
+    #         goals += 1
+    #     else:
+    #         # print("Result: No Goal")
+    #         saves += 1
+    #         # print('\n')
+    # print(saves / (saves + goals))
 
 
 if __name__ == '__main__':
